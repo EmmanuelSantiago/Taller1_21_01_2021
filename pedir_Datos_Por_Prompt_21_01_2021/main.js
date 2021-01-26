73ae69e63d7ae38f9b6246d0f302d138543396ad
Emmanuel Santiago <biosinx@gmail.com>
2021-01-25 19:18:53 -0500

    Taller1-21_01_2021

---
+++ pedir_Datos_Por_Prompt_21_01_2021/main.js
@@ -0,0 +1,5 @@
+function inicio(p1){
+    alert('Su numero es ' + p1);
+}
+let data_us = prompt('Ingresar un numero');
+inicio(data_us);
---
+++ todo_Sobre_Variables_21_01_2021/main.js
@@ -0,0 +1,15 @@
+function cierre(){
+    alert('Ver la consola del navegador o oprima la tecla F12 para ver los resultados');
+}
+cierre();
+
+let texto = 'Esto es un string, el bool lo garantiza';
+let numero = 100001;
+let boleano = true;
+
+const pato = 'Los patos vuelan, caminan y nadan';
+
+console.log(typoof(texto), texto)
+console.log(typeof(numero), numero)
+console.log(typeof(boleano), boleano)
+console.log(typeof(pato), pato)
